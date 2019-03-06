ac5580f3638dd9729ad151c3159415c883c12c9c
ThatMan <miamoro1y@163.com>
2019-03-06 23:33:30 +0800

    Initialization

--- src/main/java/com/thatman/testservice/Entity/User.java
+++ src/main/java/com/thatman/testservice/Entity/User.java
@@ -1,12 +1,9 @@
 package com.thatman.testservice.Entity;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
 public class User {
-    public @Value(value = "${userName}") String userName;
-    public @Value(value = "${userPassword}")  String userPassword;
+    public String userName;
+    public String userPassword;
 }
