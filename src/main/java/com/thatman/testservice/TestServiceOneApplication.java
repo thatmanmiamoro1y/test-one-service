ba090416c1ed8f1f355d1b896e637d65c6b718bc
ThatMan <miamoro1y@163.com>
2019-03-10 21:44:06 +0800

    Initialization

--- src/main/java/com/thatman/testservice/TestServiceOneApplication.java
+++ src/main/java/com/thatman/testservice/TestServiceOneApplication.java
@@ -3,17 +3,14 @@ package com.thatman.testservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-//@EnableEurekaClient
-//@EnableFeignClients
-@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan(basePackages = "com.thatman.testservice.mapper")
 public class TestServiceOneApplication {
 
